5a24329e0565c9d238925230310018c7616c1ea1
Ren_Ji_Wei <13575089145@qq.com>
2017-01-03 19:59:01 +0800

    di

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -3,12 +3,10 @@ package com.example.activitytest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -21,39 +19,24 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent("123456");
-                startActivity(intent);
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+               startActivityForResult(intent,1);
 
             }
         });
     }
 
-    /**
-     * 菜单
-     * @param menu
-     * @return
-     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
-    }
-
-    /**
-     * 菜单监听
-     * @param item
-     * @return
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.add_item:
-                Toast.makeText(FirstActivity.this,"点击了添加菜单",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.Remove_item:
-                Toast.makeText(FirstActivity.this,"点击了删除菜单",Toast.LENGTH_SHORT).show();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String res=data.getStringExtra("a");
+                    Log.i("msg",res);
+                }
                 break;
         }
-        return true;
     }
+
+
 }
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -1,13 +1,35 @@
 package com.example.activitytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private Button bt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+//        bt2 = (Button) findViewById(R.id.button_2);
+//        bt2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                  Intent intent = new Intent();
+//        intent.putExtra("a", "你好啊！DIYIGE FBEJ WBFKJW B");
+//        setResult(RESULT_OK, intent);
+//        finish();
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("a", "你好啊！DIYIGE FBEJ WBFKJW B");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
